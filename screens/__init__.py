9c2b10f1ca89ce88aea39417d99c39f50ae51b98
GickiAnarchy <mindofmagick85@gmail.com>
2026-03-03 14:58:55 -0600

    Cleaning up and orginizing

--- screens/__init__.py
+++ screens/__init__.py
@@ -1,3 +1,10 @@
 from .inputscreen import InputScreen
 from .readingscreen import ReadingScreen
 from .keyscreen import KeyScreen
+
+
+ALL_SCREENS = [
+    (InputScreen, "input"),
+    (ReadingScreen, "reading"),
+    (KeyScreen, "key"),
+]
--- wtutils.py
+++ wtutils.py
@@ -1,8 +1,34 @@
-import os
 import random
+import os
+import json
+
+
+
+class Cards:
+    """
+    Class to handle the card information."""
+    def __init__(self):
+        self.file_name = "cards.json"
+        with open(self.file_name, "r") as f:
+            data = json.load(f)
+            self.cards = {c.get('name'): c for c in data}
+
+    def get_desc(self, cards):
+        desc = [f"{c}\nKeywords: {self.cards[c].get('keywords')}" for c in cards]
+        return "-----".join(desc)
+
+    @property
+    def card_names(self):
+        return list(self.cards.keys())
+
+
+
 
 def get_image():
-    img_dir = "./img/"
+    """
+    Returns an image from the local img directory.
+    """
+    img_dir = "./data/img/"
     files = os.listdir(img_dir)
     images = [
         f for f in files
